d8431732ae5c45e169dae685323f02c9ee71913f
Hmz1hb <hamzalachehabe@gmail.com>
2023-02-17 16:39:41 +0100

    log-in update + style fix

--- login page/Log-in.php
+++ login page/Log-in.php
@@ -1,3 +1,5 @@
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,10 +9,10 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
   <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet"/>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous" defer></script>
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet"/>
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.1.0/mdb.min.js" defer></script>
   <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.1.0/mdb.min.css" rel="stylesheet"/>
+  <scrip type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.1.0/mdb.min.js" defer></scrip>
   <style>
     .card-registration .select-input.form-control[readonly]:not([disabled]) {
 font-size: 1rem;
@@ -21,48 +23,92 @@ padding-right: .75em;
 .card-registration .select-arrow {
 top: 13px;
 }
+
   </style>
 </head>
 <body>
-  <form class="h-100" style="background-color: #55595c ;">
-  <div class="container py-5 h-100">
-    <div class="row d-flex justify-content-center align-items-center h-100">
-      <div class="col">
-        <div class="card card-registration my-4">
-          <div class="row g-0">
-            <div class="col-xl-6 d-none d-xl-block">
-              <img src="./Img/pexels-mark-mccammon-1080696.jpg"
-                alt="Sample photo" class="img-fluid"
-                style="border-top-left-radius: .25rem; border-bottom-left-radius: .25rem;" />
-            </div>
-            <div class="col-xl-6">
-              <div class="card-body p-md-5 text-black">
-              <img src="./Img/boul-removebg-preview.png" alt="" srcset="" width="30%" class="position-relative top-0 start-50 translate-middle pt-5">
-                <h3 class="mb-5 text-center">Log In</h3>
+  <form class="h-100" style="background-color: #55595c ;" action="" method="post">
+      <div class="container py-5 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+          <div class="col">
+            <div class="card card-registration my-4">
+              <div class="row g-0">
+                <div class="col-xl-6 d-none d-xl-block">
+                  <img src="./Img/pexels-mark-mccammon-1080696.jpg"
+                    alt="Sample photo" class="img-fluid"
+                    style="border-top-left-radius: .25rem; border-bottom-left-radius: .25rem;" />
+                </div>
+                <div class="col-xl-6">
+                  <div class="card-body p-md-5 text-black">
+                  <img src="./Img/boul-removebg-preview.png" alt="" srcset="" width="30%" class="position-relative top-0 start-50 translate-middle pt-5">
+                    <h3 class="mb-5 text-center">Log In</h3>
 
-                <div class="form-outline mb-4">
-                  <input type="email" id="Email_Inp" class="form-control form-control-lg" required/>
-                  <label class="form-label" for="Email_Inp">Email</label>
-                </div>
+                    <div class="form-outline mb-4">
+                      <input type="email" id="Email_Inp" class="form-control form-control-lg" name="email"required >
+                      <label class="form-label" for="Email_Inp">Email</label>
+                    </div>
 
-                <div class="form-outline mb-4">
-                  <input type="password" id="Password_Inp" class="form-control form-control-lg" required />
-                  <label class="form-label" for="Password_Inp">Password</label>
+                    <div class="form-outline mb-4">
+                      <input type="password" id="Password_Inp" class="form-control form-control-lg" name="password" required >
+                      <label class="form-label" for="Password_Inp">Password</label>
+                    </div>
+                    
+                    <div class="d-flex justify-content-end pt-3">
+                      <button type="submit" class="btn btn-danger btn-block" >Log In</button>
+                    </div>
+
+      
+                    <div class="text-center mt-3 small">
+                        Don't have an account? <a href="../sigh-up page/signup.php">Sign Up</a>
+                      </div>
+                  </div>
                 </div>
-                
-                <div class="d-flex justify-content-end pt-3">
-                  <button type="submit" class="btn btn-danger btn-block">Log In</button>
-                </div>
-                <div class="text-center mt-3 small">
-										Don't have an account? <a href="../sigh-up page/signup.php">Sign Up</a>
-									</div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
 </form>
+<?php
+
+
+if (isset($_POST['email']) && isset($_POST['password']) ) {
+
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    try {
+            $conn = new PDO("mysql:host=localhost;dbname=gestionimmobilier;port=3306;charset=UTF8", 'root', '');
+          
+
+            $stmt = $conn->prepare("SELECT * FROM `member` WHERE `Email_M`=:Email and `MembrePasse`=:Pass");
+            $stmt->bindParam(':Email', $email);
+            $stmt->bindParam(':Pass', $password);
+            $stmt->execute();
+            
+            // Vérifier si l'authentification est réussie
+            if ($stmt->rowCount() > 0) {
+                // Stocker les informations de l'utilisateur dans une session
+                session_start();
+                $_SESSION['user'] = $stmt->fetch();
+            
+                // Rediriger l'utilisateur vers la page d'accueil
+                header("Location: index.php");
+                exit;
+            } else {
+                // Afficher un message d'erreur personnalisé selon le type d'erreur
+                $error = "Email ou mot de passe incorrect.";
+                echo $error;
+                // ou $error = "Compte non activé."; ou $error = "Compte bloqué.";
+            }
+            
+
+        } catch (PDOException $e) {
+            $errorMessage = "Error:" . $e->getMessage();
+        }
+    } 
+?>
+
+
 </body>
 </html>
