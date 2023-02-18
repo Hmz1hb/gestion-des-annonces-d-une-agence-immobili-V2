4e483ef54a74411abb20e7b151c2d05b6a45fe8f
Hmz1hb <hamzalachehabe@gmail.com>
2023-02-18 18:26:06 +0100

    details.php

---
+++ Page Visiteure/details.php
@@ -0,0 +1,135 @@
+
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <title>Boulevard</title>
+
+  <link rel="canonical" href="https://v5.getbootstrap.com/docs/5.0/examples/album/">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous" defer></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js" integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<link rel="stylesheet" href="./style.css?v=<?php echo time(); ?>">
+
+</head>
+
+<body>
+
+  <header>
+    <div class="collapse" id="navbarHeader" style="background-color: #55595c;">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-8 col-md-7 py-4">
+            <h4 class="text-white">About</h4>
+            <p class="text-white">Boulevard Real Estate is a full-service real estate agency committed to providing exceptional service to all of our clients. Our experienced agents have a deep understanding of the local real estate market and are equipped to assist with all your buying, selling, and renting needs. From the initial search to closing the deal, we work closely with our clients to ensure a smooth and successful transaction. With our network of industry professionals and cutting-edge technology, we are able to provide our clients with a competitive edge in today's fast-paced market. Whether you're looking to purchase your dream home, sell your current property, or rent a new place, Boulevard Real Estate is here to help. Contact us today to learn how we can assist you with your real estate needs.</p>
+          </div>
+          <div class="col-sm-4 offset-md-1 py-4">
+            <h4 class="text-white">Contact</h4>
+            <ul class="list-unstyled">
+              <li><a href="#" class="text-white">Follow on Twitter</a></li>
+              <li><a href="#" class="text-white">Like on Facebook</a></li>
+              <li><a href="#" class="text-white">Email me</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="navbar navbar-dark bg-light shadow-sm">
+      <div class="container" id>
+        <a href="#" class="navbar-brand d-flex align-items-center">
+          <img src="./img/boul-removebg-preview.png" alt="Boulvard" width="30%"  fill="none"> 
+        </a>
+        <button"><i class="fa-solid fa-user-plus"></i></button>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <i class="fa-solid fa-bars" style="color: #55595c;"></i>
+        </button>
+      </div>
+
+      
+    </div>
+  </header>
+
+  <main>
+
+    <section class="py-5 text-center container">
+        <div id="carouselExampleCaptions" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="../img/appartement11.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="../img/appartement12.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+      </div>
+    </section>
+
+    <div class="album py-5 bg-light">
+      <div class="container">
+      <div class="single-post">
+       <div class="feature-img">
+        <img class="img-fluid" src="assets/img/blog/single_blog_1.png" alt="">
+      </div>
+      <div class="blog_details">
+        <h2 style="color: #2d2d2d;">Announce title</h2>
+       <p class="excert mt-5">
+        Announce details here
+       </p>
+       <p>
+         MCSE boot camps have its supporters and its detractors. Some people do not understand why you
+         should have to spend money on boot camp when you can get the MCSE study materials yourself at a
+         fraction of the camp price. However, who has the willpower to actually sit through a
+         self-imposed MCSE training. who has the willpower to actually
+       </p>
+       <div class="quote-wrapper">
+         <div class="quotes">
+          MCSE boot camps have its supporters and its detractors. Some people do not understand why you
+          should have to spend money on boot camp when you can get the MCSE study materials yourself at
+          a fraction of the camp price. However, who has the willpower to actually sit through a
+          self-imposed MCSE training.
+        </div>
+      </div>
+      <p>
+       MCSE boot camps have its supporters and its detractors. Some people do not understand why you
+       should have to spend money on boot camp when you can get the MCSE study materials yourself at a
+       fraction of the camp price. However, who has the willpower
+     </p>
+     <p>
+       MCSE boot camps have its supporters and its detractors. Some people do not understand why you
+       should have to spend money on boot camp when you can get the MCSE study materials yourself at a
+       fraction of the camp price. However, who has the willpower to actually sit through a
+       self-imposed MCSE training. who has the willpower to actually
+     </p>
+   </div>
+ </div>
+      </div>
+    </div>
+  </main>
+  <footer class="text-muted py-5">
+    <div class="container">
+      <p class="float-right mb-1">
+        <a href="#"><i class="fa-solid fa-arrow-up" style="color: #55595c;"></i></a>
+    </div>
+  </footer>
+<script src="./java.js"></script>
+</body>
+
+</html> 
