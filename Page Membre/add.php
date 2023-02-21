9b8a97f4716d053d1b8de45cad82e2d08d745bbf
NadaAberrada <aberradanada@gmail.com>
2023-02-21 16:30:45 +0100

    add page

--- Page Membre/add.php
+++ Page Membre/add.php
@@ -8,26 +8,33 @@
 </head>
 <body>
 
-<form   action="" method="post">
+<form   action="" method="post" enctype="multipart/form-data">
 
         <label for="titre"><Title></Title></label>
           <input type="text" class="form-control" id="titre" name="titre"><br>
 
 
           <label for="img">image</label>
-          <input type="file" class="form-control" id="image" name="image"><br>
+          <input type="file" name="images[]"id="image" multiple><br>
+       
 
           <label for="adresse">Adresse</label>
           <input type="text" class="form-control" id="adresse" name="adresse"><br>
 
           <label for="montant">price</label>
-          <input type="number" class="form-control" id="montant" name="montant"><br>
+          <input type="number"  step="0.01" class="form-control" id="montant" name="montant"><br>
 
           <label for="dateAnnonce">Date PUB</label>
           <input type="date" class="form-control" id="dateAnnonce" name="dateP"><br>
+          <select name="type" class="form-control search-slt" id="exampleFormControlSelect1">
+                        <option value="#">Type</option>
+                        <option value="maison">Maison</option>
+                        <option value="appartement">Appartement</option>
+                        <option value="terrain">Terrain</option>
+                        <option value="bureau">bureau</option>
+                      </select>
 
-
-          <select class="form-control" id="type" name="type"><br>
+          <select class="form-control" id="category" name="category"><br>
             <option>Vente</option>
             <option>Location</option>
           </select>
@@ -39,7 +46,7 @@
 
    
         session_start();
-    echo  $_SESSION['id'];
+    $sessionId= $_SESSION['id'];
 
 if (
     isset($_POST['titre']) && isset($_POST['image']) 
@@ -48,48 +55,89 @@ if (
 ) {
     $TitreContent = $_POST['titre'];
     $adresseContent = $_POST['adresse'];
-    $montantContent = $_POST['montant'];
+    $montantContent= number_format($_POST['prix'], 2, '.', '');
     $datePContent = $_POST['dateAnnonce'];
     $typeContent = $_POST['type'];
+    $categoryContent = $_POST['category'];
 
     if (!empty($_FILES['image']['name'])) {
-        $file_name = $_FILES['image']['name'];
-        $file_tmp = $_FILES['image']['tmp_name'];
-        $file_type = $_FILES['image']['type'];
-        $file_size = $_FILES['image']['size'];
-        $new_file_name = "default.jpg";
+        $file_count = count($_FILES['images']['name']);
 
-        // Allowed file types
-        $allowed_types = array("image/jpeg", "image/png", "image/gif");
-
-        // Check if the uploaded file is of the allowed type
-        if (!in_array($file_type, $allowed_types)) {
-            // Handle error: Invalid file type
-        }
-
-        // Set a maximum file size limit
-        $max_size = 5000000; // 5MB
-
-        // Check if the uploaded file size is within the limit
-        if ($file_size > $max_size) {
-            // Handle error: File size too large
-        }
-
-        // Generate a unique file name
-        $new_file_name_random = time() . "-" . $file_name;
-        $new_file_name = "./images/" . $new_file_name_random;
-
-
-        // Move the uploaded file to a permanent location
-        $upload_path = "./images/" . $new_file_name;
-        if (move_uploaded_file($file_tmp, $upload_path)) {
-            // Upload successful
-        } else {
-            // Handle error: Upload failed
-        }
-    } else {
-        // Handle error: No file uploaded
+        for ($i = 0; $i < $file_count; $i++) {
+            $file_name = $_FILES['images']['name'][$i];
+            $file_tmp = $_FILES['images']['tmp_name'][$i];
+            $file_type = $_FILES['images']['type'][$i];
+            $file_size = $_FILES['images']['size'][$i];
+            $new_file_name = "default.jpg";
+    
+            // Allowed file extensions
+            $allowed_extensions = array("jpg", "jpeg", "png", "gif");
+    
+            // Get the file extension of the uploaded file
+            $file_ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
+    
+            // Check if the uploaded file extension is allowed
+            if (!in_array($file_ext, $allowed_extensions)) {
+                // Handle error: Invalid file extension
+            }
+    
+            // Set a maximum file size limit
+            $max_size = 5000000; // 5MB
+    
+            // Check if the uploaded file size is within the limit
+            if ($file_size > $max_size) {
+                // Handle error: File size too large
+            }
+    
+            // Validate the uploaded file before storing it
+            $image_info = getimagesize($file_tmp);
+            if (!$image_info) {
+                // Handle error: Invalid image file
+            }
+    
+            // Generate a unique file name
+            $new_file_name_random = time() . "-" . $file_name;
+            $new_file_path = "./images/" . $new_file_name_random;
+    
+            // Store the uploaded file
+            if (!move_uploaded_file($file_tmp, $new_file_path)) {
+                // Handle error: Failed to store file
+            }
+    
+            // Determine if the file is the principal image
+            $is_principal = ($i == 0 && isset($_POST['principal_image']) && $_POST['principal_image'] == $i) ? 1 : 0;
+    
+            // Set the Image_P value
+            $image_p = ($is_principal == 1) ? "OUI" : "NON";
+            try {
+                $conn = new PDO("mysql:host=localhost;dbname=gestionimmobilier;port=3306;charset=UTF8", 'root', '');
+                // set the PDO error mode to exception
+                 
+                $stmt = $conn->prepare("INSERT INTO announcement (Titre, Prix, DatePub, LastModified, adresse, Category, Type, Membre_ID) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+                $stmt->bindParam(1, $TitreContent);
+                $stmt->bindParam(2, $montantContent);
+                $stmt->bindParam(3, date("Y/m/d"));
+                $stmt->bindParam(4, date("Y/m/d"));
+                $stmt->bindParam(5, $adresseContent);
+                $stmt->bindParam(6, $categoryContent);
+                $stmt->bindParam(7, $typeContent);
+                $stmt->bindParam(8, $sessionId);    
+                $stmt->execute();
+                $order_id = $conn->lastInsertId();
+            
+                $stmt = $conn->prepare("INSERT INTO image (url, announcement_id, is_principal) VALUES (?, ?, ?)");
+                $stmt->bindParam(1, $new_file_name_random);
+                $stmt->bindParam(2, $order_id);
+                $stmt->bindParam(3, $image_p);    
+                $stmt->execute();
+            } catch (PDOException $e) {
+                echo "Error: " . $e->getMessage();
+            }
     }
+}            
+
+
+
 
 
 
